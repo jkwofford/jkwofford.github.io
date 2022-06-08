7aec55a9521749141a6d58f518bf4f9728a7928e
Jared Wofford <jkwoffor@gmail.com>
2022-06-08 03:20:27 -0400

    Update O3ScoX1CrossCorr_figs.py

--- O3ScoX1CrossCorr_figs.py
+++ O3ScoX1CrossCorr_figs.py
@@ -1,4 +1,4 @@
-<mxfile host="app.diagrams.net" modified="2022-06-08T07:19:47.360Z" agent="5.0 (X11)" etag="XDuomf97OrApTLaTIBpH" version="16.5.6" type="github">
+<mxfile host="app.diagrams.net" modified="2022-06-08T07:20:26.106Z" agent="5.0 (X11)" etag="gCxhRWwhAX17duoa0szV" version="16.5.6" type="github">
   <diagram id="xByAA2xhnRinG42e1BB1" name="Page-1">
     <mxGraphModel dx="7435" dy="4040" grid="0" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="0" pageScale="1" pageWidth="850" pageHeight="1100" math="0" shadow="0">
       <root>
@@ -7,10 +7,10 @@
         <mxCell id="Gp2SxNCE1c6KaDwu4bxu-34" value="&lt;div style=&quot;font-size: 60px&quot;&gt;Sco X1 Right Ascension&lt;br style=&quot;font-size: 60px&quot;&gt;&lt;/div&gt;&lt;div style=&quot;font-size: 60px&quot;&gt;&lt;code style=&quot;font-size: 60px&quot;&gt;&lt;span class=&quot;n&quot; style=&quot;font-size: 60px&quot;&gt;Sco X1 Declination&lt;br style=&quot;font-size: 60px&quot;&gt;&lt;/span&gt;&lt;/code&gt;&lt;/div&gt;" style="html=1;strokeWidth=4;shape=manualInput;whiteSpace=wrap;rounded=1;size=26;arcSize=11;labelBackgroundColor=#FFFFFF;fontSize=60;fontColor=#000000;fontStyle=0;shadow=1;" parent="1" vertex="1">
           <mxGeometry x="-2415" y="930" width="1190" height="730" as="geometry" />
         </mxCell>
-        <mxCell id="Gp2SxNCE1c6KaDwu4bxu-35" value="&lt;b&gt;&lt;span style=&quot;font-size: 60px&quot;&gt;&lt;font style=&quot;font-size: 60px&quot;&gt;O3 start/end times&lt;/font&gt;&lt;/span&gt;&lt;/b&gt;" style="html=1;strokeWidth=4;shape=manualInput;whiteSpace=wrap;rounded=1;size=26;arcSize=11;labelBackgroundColor=#FFFFFF;fontSize=60;fontColor=#000000;fontStyle=0;shadow=1;" parent="1" vertex="1">
+        <mxCell id="Gp2SxNCE1c6KaDwu4bxu-35" value="&lt;b&gt;&lt;span style=&quot;font-size: 60px&quot;&gt;&lt;font style=&quot;font-size: 60px&quot;&gt;O3 start/end times&lt;/font&gt;&lt;/span&gt;&lt;/b&gt;" style="html=1;strokeWidth=7;shape=manualInput;whiteSpace=wrap;rounded=1;size=26;arcSize=11;labelBackgroundColor=#FFFFFF;fontSize=25;fontColor=#000000;fontStyle=0;shadow=1;perimeter=rectanglePerimeter;" parent="1" vertex="1">
           <mxGeometry x="-795" y="85" width="660" height="460" as="geometry" />
         </mxCell>
-        <mxCell id="Gp2SxNCE1c6KaDwu4bxu-36" value="&lt;b&gt;&lt;font style=&quot;font-size: 60px&quot;&gt;O3 start/end times&lt;/font&gt;&lt;/b&gt;" style="html=1;strokeWidth=2;shape=manualInput;whiteSpace=wrap;rounded=1;size=26;arcSize=11;labelBackgroundColor=#FFFFFF;fontSize=25;fontColor=#000000;" parent="1" vertex="1">
+        <mxCell id="Gp2SxNCE1c6KaDwu4bxu-36" value="&lt;b&gt;&lt;font style=&quot;font-size: 60px&quot;&gt;O3 start/end times&lt;/font&gt;&lt;/b&gt;" style="html=1;strokeWidth=7;shape=manualInput;whiteSpace=wrap;rounded=1;size=26;arcSize=11;labelBackgroundColor=#FFFFFF;fontSize=25;fontColor=#000000;" parent="1" vertex="1">
           <mxGeometry x="-1708" y="95" width="680" height="450" as="geometry" />
         </mxCell>
       </root>
