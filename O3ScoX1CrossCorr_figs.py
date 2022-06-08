e8e03c227e76029a3ca1fdcd634330792f30b586
Jared Wofford <jkwoffor@gmail.com>
2022-06-08 03:21:48 -0400

    Update O3ScoX1CrossCorr_figs.py

--- O3ScoX1CrossCorr_figs.py
+++ O3ScoX1CrossCorr_figs.py
@@ -1,11 +1,11 @@
-<mxfile host="app.diagrams.net" modified="2022-06-08T07:20:26.106Z" agent="5.0 (X11)" etag="gCxhRWwhAX17duoa0szV" version="16.5.6" type="github">
+<mxfile host="app.diagrams.net" modified="2022-06-08T07:21:47.093Z" agent="5.0 (X11)" etag="rHRQdh3IAFmJtrxiP6p-" version="16.5.6" type="github">
   <diagram id="xByAA2xhnRinG42e1BB1" name="Page-1">
-    <mxGraphModel dx="7435" dy="4040" grid="0" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="0" pageScale="1" pageWidth="850" pageHeight="1100" math="0" shadow="0">
+    <mxGraphModel dx="6635" dy="4440" grid="0" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="0" pageScale="1" pageWidth="850" pageHeight="1100" math="0" shadow="0">
       <root>
         <mxCell id="0" />
         <mxCell id="1" parent="0" />
-        <mxCell id="Gp2SxNCE1c6KaDwu4bxu-34" value="&lt;div style=&quot;font-size: 60px&quot;&gt;Sco X1 Right Ascension&lt;br style=&quot;font-size: 60px&quot;&gt;&lt;/div&gt;&lt;div style=&quot;font-size: 60px&quot;&gt;&lt;code style=&quot;font-size: 60px&quot;&gt;&lt;span class=&quot;n&quot; style=&quot;font-size: 60px&quot;&gt;Sco X1 Declination&lt;br style=&quot;font-size: 60px&quot;&gt;&lt;/span&gt;&lt;/code&gt;&lt;/div&gt;" style="html=1;strokeWidth=4;shape=manualInput;whiteSpace=wrap;rounded=1;size=26;arcSize=11;labelBackgroundColor=#FFFFFF;fontSize=60;fontColor=#000000;fontStyle=0;shadow=1;" parent="1" vertex="1">
-          <mxGeometry x="-2415" y="930" width="1190" height="730" as="geometry" />
+        <mxCell id="Gp2SxNCE1c6KaDwu4bxu-34" value="&lt;div align=&quot;justify&quot;&gt;&lt;blockquote&gt;&lt;blockquote&gt;&lt;blockquote&gt;&lt;div style=&quot;font-size: 60px&quot;&gt;&lt;b&gt;Sco X1 Right Ascension&lt;/b&gt;&lt;br style=&quot;font-size: 60px&quot;&gt;&lt;/div&gt;&lt;div style=&quot;font-size: 60px&quot;&gt;&lt;b&gt;&lt;code style=&quot;font-size: 60px&quot;&gt;&lt;span class=&quot;n&quot; style=&quot;font-size: 60px&quot;&gt;Sco X1 Declination&lt;/span&gt;&lt;/code&gt;&lt;/b&gt;&lt;br style=&quot;font-size: 60px&quot;&gt;&lt;b&gt;&lt;code style=&quot;font-size: 60px&quot;&gt;&lt;span class=&quot;n&quot; style=&quot;font-size: 60px&quot;&gt;&lt;/span&gt;&lt;/code&gt;&lt;/b&gt;&lt;/div&gt;&lt;/blockquote&gt;&lt;/blockquote&gt;&lt;/blockquote&gt;&lt;/div&gt;" style="html=1;strokeWidth=7;shape=manualInput;whiteSpace=wrap;rounded=1;size=173;arcSize=0;labelBackgroundColor=#FFFFFF;fontSize=25;fontColor=#000000;fontStyle=0;shadow=1;align=left;" parent="1" vertex="1">
+          <mxGeometry x="650" y="-245.37" width="970" height="876.37" as="geometry" />
         </mxCell>
         <mxCell id="Gp2SxNCE1c6KaDwu4bxu-35" value="&lt;b&gt;&lt;span style=&quot;font-size: 60px&quot;&gt;&lt;font style=&quot;font-size: 60px&quot;&gt;O3 start/end times&lt;/font&gt;&lt;/span&gt;&lt;/b&gt;" style="html=1;strokeWidth=7;shape=manualInput;whiteSpace=wrap;rounded=1;size=26;arcSize=11;labelBackgroundColor=#FFFFFF;fontSize=25;fontColor=#000000;fontStyle=0;shadow=1;perimeter=rectanglePerimeter;" parent="1" vertex="1">
           <mxGeometry x="-795" y="85" width="660" height="460" as="geometry" />
