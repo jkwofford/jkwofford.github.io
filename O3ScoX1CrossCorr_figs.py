6e3a106f191e8d0c766a01ae5ed21d97b4b732d1
Jared Wofford <jkwoffor@gmail.com>
2022-06-08 03:25:55 -0400

    Update O3ScoX1CrossCorr_figs.py

--- O3ScoX1CrossCorr_figs.py
+++ O3ScoX1CrossCorr_figs.py
@@ -1,6 +1,6 @@
-<mxfile host="app.diagrams.net" modified="2022-06-08T07:21:47.093Z" agent="5.0 (X11)" etag="rHRQdh3IAFmJtrxiP6p-" version="16.5.6" type="github">
+<mxfile host="app.diagrams.net" modified="2022-06-08T07:25:54.366Z" agent="5.0 (X11)" etag="vhS9lFtOK-ME5gYcySII" version="16.5.6" type="github">
   <diagram id="xByAA2xhnRinG42e1BB1" name="Page-1">
-    <mxGraphModel dx="6635" dy="4440" grid="0" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="0" pageScale="1" pageWidth="850" pageHeight="1100" math="0" shadow="0">
+    <mxGraphModel dx="15780" dy="8187" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="0" pageScale="1" pageWidth="850" pageHeight="1100" math="0" shadow="1">
       <root>
         <mxCell id="0" />
         <mxCell id="1" parent="0" />
@@ -12,6 +12,18 @@
         </mxCell>
         <mxCell id="Gp2SxNCE1c6KaDwu4bxu-36" value="&lt;b&gt;&lt;font style=&quot;font-size: 60px&quot;&gt;O3 start/end times&lt;/font&gt;&lt;/b&gt;" style="html=1;strokeWidth=7;shape=manualInput;whiteSpace=wrap;rounded=1;size=26;arcSize=11;labelBackgroundColor=#FFFFFF;fontSize=25;fontColor=#000000;" parent="1" vertex="1">
           <mxGeometry x="-1708" y="95" width="680" height="450" as="geometry" />
+        </mxCell>
+        <mxCell id="I19uzROf9nRGAKfcDDE6-2" value="latexscinot" style="swimlane;shadow=1;fontSize=120;strokeWidth=7;startSize=590;fillColor=#dae8fc;strokeColor=#6c8ebf;" vertex="1" parent="1">
+          <mxGeometry x="-8070" y="-2770" width="1620" height="2120" as="geometry" />
+        </mxCell>
+        <mxCell id="I19uzROf9nRGAKfcDDE6-3" value="h0effsq_by_h0sq_old(cosisq)" style="swimlane;shadow=1;fontSize=100;strokeWidth=7;startSize=590;fillColor=#dae8fc;strokeColor=#6c8ebf;" vertex="1" parent="1">
+          <mxGeometry x="-9420" y="-20" width="1620" height="2120" as="geometry" />
+        </mxCell>
+        <mxCell id="I19uzROf9nRGAKfcDDE6-4" value="h0effsq_by_h0sq(cosisq)" style="swimlane;shadow=1;fontSize=100;strokeWidth=7;startSize=590;fillColor=#dae8fc;strokeColor=#6c8ebf;" vertex="1" parent="1">
+          <mxGeometry x="-7180" y="-40" width="1620" height="2120" as="geometry" />
+        </mxCell>
+        <mxCell id="I19uzROf9nRGAKfcDDE6-5" value="parseMetadata" style="swimlane;shadow=1;fontSize=100;strokeWidth=7;startSize=590;fillColor=#dae8fc;strokeColor=#6c8ebf;" vertex="1" parent="1">
+          <mxGeometry x="-8210" y="3240" width="1620" height="2120" as="geometry" />
         </mxCell>
       </root>
     </mxGraphModel>
