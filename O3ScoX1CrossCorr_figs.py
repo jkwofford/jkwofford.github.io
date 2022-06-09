0a39f79ebce001567e611cd3ce0bcc7a731494a3
Jared Wofford <jkwoffor@gmail.com>
2022-06-09 15:18:51 -0400

    Update O3ScoX1CrossCorr_figs.py

--- O3ScoX1CrossCorr_figs.py
+++ O3ScoX1CrossCorr_figs.py
@@ -1,6 +1,6 @@
-<mxfile host="app.diagrams.net" modified="2022-06-09T19:09:29.914Z" agent="5.0 (X11)" etag="D7lRTyGcC1s7G5rwhzP8" version="16.5.6" type="github">
+<mxfile host="app.diagrams.net" modified="2022-06-09T19:18:50.478Z" agent="5.0 (X11)" etag="tkE1cPrzWpoIXo5KHJDf" version="16.5.6" type="github">
   <diagram id="xByAA2xhnRinG42e1BB1" name="Page-1">
-    <mxGraphModel dx="28140" dy="19360" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="0" pageScale="1" pageWidth="850" pageHeight="1100" math="0" shadow="1">
+    <mxGraphModel dx="18870" dy="11280" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="0" pageScale="1" pageWidth="850" pageHeight="1100" math="0" shadow="1">
       <root>
         <mxCell id="0" />
         <mxCell id="1" parent="0" />
@@ -70,11 +70,45 @@
         <mxCell id="Tffpq6LBBL-PdYRvwtU_-93" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;entryX=0;entryY=0.5;entryDx=0;entryDy=0;fontSize=200;fontColor=#FF3333;strokeWidth=50;" edge="1" parent="1" source="Tffpq6LBBL-PdYRvwtU_-85" target="Tffpq6LBBL-PdYRvwtU_-92">
           <mxGeometry relative="1" as="geometry" />
         </mxCell>
+        <mxCell id="Tffpq6LBBL-PdYRvwtU_-95" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;fontSize=200;fontColor=#FF3333;strokeWidth=50;" edge="1" parent="1" source="Tffpq6LBBL-PdYRvwtU_-85" target="Tffpq6LBBL-PdYRvwtU_-94">
+          <mxGeometry relative="1" as="geometry" />
+        </mxCell>
         <mxCell id="Tffpq6LBBL-PdYRvwtU_-85" value="&lt;font style=&quot;font-size: 200px&quot;&gt;Read K1, Tsigma&lt;br&gt;&lt;/font&gt;" style="rounded=1;whiteSpace=wrap;html=1;fontFamily=Helvetica;fontSize=12;fontColor=rgb(0, 0, 0);align=center;strokeColor=rgb(0, 0, 0);strokeWidth=2;fillColor=rgb(255, 255, 255);" vertex="1" parent="1">
           <mxGeometry x="-2830" y="10128" width="2980" height="1945" as="geometry" />
         </mxCell>
         <mxCell id="Tffpq6LBBL-PdYRvwtU_-92" value="Orbital Params" style="swimlane;rounded=1;fontSize=200;fontColor=#FF3333;strokeWidth=20;startSize=540;" vertex="1" parent="1">
           <mxGeometry x="2160" y="10080" width="1920" height="1760" as="geometry" />
+        </mxCell>
+        <mxCell id="Tffpq6LBBL-PdYRvwtU_-102" value="" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;fontSize=200;fontColor=#FF3333;strokeWidth=50;" edge="1" parent="1" source="Tffpq6LBBL-PdYRvwtU_-94" target="Tffpq6LBBL-PdYRvwtU_-101">
+          <mxGeometry relative="1" as="geometry" />
+        </mxCell>
+        <mxCell id="Tffpq6LBBL-PdYRvwtU_-94" value="&lt;font style=&quot;font-size: 200px&quot;&gt;mask cells for Tmax from O1 Tmax File&lt;br&gt;&lt;/font&gt;" style="rounded=1;whiteSpace=wrap;html=1;fontFamily=Helvetica;fontSize=12;fontColor=rgb(0, 0, 0);align=center;strokeColor=rgb(0, 0, 0);strokeWidth=2;fillColor=rgb(255, 255, 255);" vertex="1" parent="1">
+          <mxGeometry x="-2830" y="12648" width="2980" height="1945" as="geometry" />
+        </mxCell>
+        <mxCell id="Tffpq6LBBL-PdYRvwtU_-104" value="Yes" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;fontSize=200;fontColor=#FF3333;strokeWidth=50;" edge="1" parent="1" source="Tffpq6LBBL-PdYRvwtU_-101" target="Tffpq6LBBL-PdYRvwtU_-103">
+          <mxGeometry relative="1" as="geometry" />
+        </mxCell>
+        <mxCell id="Tffpq6LBBL-PdYRvwtU_-105" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;entryX=-0.013;entryY=0.586;entryDx=0;entryDy=0;entryPerimeter=0;fontSize=200;fontColor=#FF3333;strokeWidth=50;" edge="1" parent="1" source="Tffpq6LBBL-PdYRvwtU_-101" target="Tffpq6LBBL-PdYRvwtU_-2">
+          <mxGeometry relative="1" as="geometry">
+            <Array as="points">
+              <mxPoint x="-5060" y="16464" />
+              <mxPoint x="-5060" y="8930" />
+              <mxPoint x="-5080" y="8930" />
+              <mxPoint x="-5080" y="1460" />
+            </Array>
+          </mxGeometry>
+        </mxCell>
+        <mxCell id="Tffpq6LBBL-PdYRvwtU_-101" value="&lt;font style=&quot;font-size: 200px&quot;&gt;Loop done?&lt;/font&gt;" style="rhombus;whiteSpace=wrap;html=1;fillColor=rgb(255, 255, 255);strokeColor=rgb(0, 0, 0);fontColor=rgb(0, 0, 0);rounded=1;strokeWidth=2;" vertex="1" parent="1">
+          <mxGeometry x="-3530" y="15409" width="4380" height="2111" as="geometry" />
+        </mxCell>
+        <mxCell id="Tffpq6LBBL-PdYRvwtU_-107" value="" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;fontSize=200;fontColor=#FF3333;strokeWidth=50;" edge="1" parent="1" source="Tffpq6LBBL-PdYRvwtU_-103" target="Tffpq6LBBL-PdYRvwtU_-106">
+          <mxGeometry relative="1" as="geometry" />
+        </mxCell>
+        <mxCell id="Tffpq6LBBL-PdYRvwtU_-103" value="Get Values" style="whiteSpace=wrap;html=1;aspect=fixed;fontFamily=Helvetica;fontSize=200;fontColor=rgb(0, 0, 0);align=center;strokeColor=rgb(0, 0, 0);strokeWidth=2;fillColor=rgb(255, 255, 255);" vertex="1" parent="1">
+          <mxGeometry x="-3530" y="19085" width="4380" height="2111" as="geometry" />
+        </mxCell>
+        <mxCell id="Tffpq6LBBL-PdYRvwtU_-106" value="" style="whiteSpace=wrap;html=1;fontSize=200;fillColor=rgb(255, 255, 255);strokeColor=rgb(0, 0, 0);fontColor=rgb(0, 0, 0);strokeWidth=2;" vertex="1" parent="1">
+          <mxGeometry x="2530" y="19411" width="1180" height="1459" as="geometry" />
         </mxCell>
       </root>
     </mxGraphModel>
